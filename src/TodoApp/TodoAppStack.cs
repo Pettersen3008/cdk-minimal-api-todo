9f62b84b875ef65d83482d4862b06f5a00c4b3d8
Sander Pettersen <48487730+Pettersen3008@users.noreply.github.com>
2025-01-06 13:13:42 +0100

    Initial commit

---
+++ src/TodoApp/TodoAppStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace TodoApp
+{
+    public class TodoAppStack : Stack
+    {
+        internal TodoAppStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
